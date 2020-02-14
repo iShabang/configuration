fe815eba412762cdec0144406145457f6a64a7e2
iShabang <montos2@unlv.nevada.edu>
2020-02-14 14:44:51 -0800

    adding startup script

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# This script is meant to run when you login. For Ubuntu, I put this in /etc/profile.d/ which will
+# autorun the script on login.
+
+
+feh --bg-fill ~/Pictures/817184.png
+compton --active-opacity=0.95 --inactive-opacity=0.8 -b
