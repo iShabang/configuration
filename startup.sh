a831b334e5777b8f4404f85126fb6ea2cf546258
Shabang <montos2@unlve.nevada.edu>
2020-03-30 20:05:58 -0700

    adding polybar setup

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@
 # This script is meant to run when you login. For Ubuntu, I put this in /etc/profile.d/ which will
 # autorun the script on login.
 
-
-feh --bg-fill ~/Pictures/817184.png
-compton --active-opacity=0.95 --inactive-opacity=0.8 -b
+feh --bg-fill ~/.config/wallpaper/wall.png
+compton -b
+$HOME/.config/polybar/launch.sh
